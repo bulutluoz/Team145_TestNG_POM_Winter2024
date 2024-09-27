36797e3182d058d1883d82346c2741f4a94bd60a
bulutluoz <abbulutluoz@gmail.com>
2024-09-27 19:11:56 +0200

    Ders17 sonu

---
+++ src/test/java/tests/day17_TestNGFramework_Assertions/C01_QuitDriver.java
@@ -0,0 +1,51 @@
+package tests.day17_TestNGFramework_Assertions;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C01_QuitDriver {
+
+    // 3 farkli test methodu olusturarak asagidaki gorevleri yapin
+    // 1- testotomasyonu anasayfaya gidin Title'in "Test Otomasyonu" icerdigini test edin
+    // 2- wisequarter anasayfaya gidin Url'in "wisequarter" icerdigini test edin
+    // 3- youtube anasayfaya gidin Title'in "vimeo" icermedigini test edin
+
+    @Test
+    public void testotomasyonuTesti(){
+        Driver.getDriver().get("https://www.testotomasyonu.com");
+
+        String expectedTitleIcerik = "Test Otomasyonu";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedTitleIcerik));
+        ReusableMethods.bekle(2);
+        Driver.quitDriver();
+    }
+
+    @Test
+    public void wisequarterTesti(){
+        Driver.getDriver().get("https://www.wisequarter.com");
+
+        String expectedUrlIcerik = "wisequarter";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(expectedUrlIcerik));
+        ReusableMethods.bekle(2);
+        Driver.quitDriver();
+    }
+
+    @Test
+    public void youtubeTesti(){
+
+        Driver.getDriver().get("https://www.youtube.com");
+
+        String unExpectedTitleIcerik = "vimeo";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertFalse(actualTitle.contains(unExpectedTitleIcerik));
+        ReusableMethods.bekle(2);
+        Driver.quitDriver();
+    }
+}
