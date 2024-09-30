603463257c95c03b9359f180fb7d1114fa8e6d0a
bulutluoz <abbulutluoz@gmail.com>
2024-09-30 21:00:42 +0200

    Ders18 sonu

--- src/test/java/pages/ZeroPage.java
+++ src/test/java/pages/ZeroPage.java
@@ -18,4 +18,28 @@ public class ZeroPage {
     @FindBy(id = "signin_button")
     public WebElement signInButonu;
 
+    @FindBy(id ="user_login")
+    public WebElement loginKutusu;
+
+    @FindBy(id ="user_password")
+    public WebElement passwordKutusu;
+
+    @FindBy(xpath = "//*[@value='Sign in']")
+    public WebElement signinSayfasiSignInButonu;
+
+    @FindBy (xpath = "(//*[@class='dropdown-toggle'])[1]")
+    public WebElement settingsTusu;
+
+    @FindBy(xpath = "(//*[text()='Online Banking'])[1]")
+    public WebElement onlineBankingLinki;
+
+    @FindBy(id="pay_bills_link")
+    public WebElement payBillsLinki;
+
+    @FindBy(xpath = "//*[text()='Purchase Foreign Currency']")
+    public WebElement purchaseCurrencyMenu;
+
+    @FindBy(id="pc_currency")
+    public WebElement currencyDropdownMenu;
+
 }
--- src/test/java/tests/day18_TestNGAssertions_xmlFiles/C02_ZeroSoftAssert.java
+++ src/test/java/tests/day18_TestNGAssertions_xmlFiles/C02_ZeroSoftAssert.java
@@ -1,11 +1,15 @@
 package tests.day18_TestNGAssertions_xmlFiles;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.ZeroPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+
+import java.util.List;
 
 public class C02_ZeroSoftAssert {
 
@@ -28,24 +32,63 @@ public class C02_ZeroSoftAssert {
         zeroPage.signInButonu.click();
 
         // 4. Login kutusuna “username” yazin
+        zeroPage.loginKutusu.sendKeys(ConfigReader.getProperty("zeroGecerliUsername"));
 
         // 5. Password kutusuna “password” yazin
+        zeroPage.passwordKutusu.sendKeys(ConfigReader.getProperty("zeroGecerliPassword"));
 
         // 6. Sign in tusuna basin
+        zeroPage.signinSayfasiSignInButonu.click();
 
         // 7. Back tusuna basin
+        Driver.getDriver().navigate().back();
 
         // 8. Giris yapilabildigini dogrulayin
-
+        softAssert.assertTrue(zeroPage.settingsTusu.isDisplayed());
 
         // 9. Online banking menusunu tiklayin
+        zeroPage.onlineBankingLinki.click();
+
         //10. Pay Bills sayfasina gidin
+        zeroPage.payBillsLinki.click();
+
         //11. “Purchase Foreign Currency” tusuna basin
+        zeroPage.purchaseCurrencyMenu.click();
+
         //12. Currency dropdown menusunun erisilebilir oldugunu dogrulayin
+        softAssert.assertTrue(zeroPage.currencyDropdownMenu.isEnabled());
+
         //13. “Currency” dropdown menusunden Eurozone’u secin
+        Select select = new Select(zeroPage.currencyDropdownMenu);
+
+        select.selectByValue("EUR");
+
         //14. "Eurozone (euro)" secildigini dogrulayin
+
+        String expectedOption = "Eurozone (euro)";
+        String actualSecilenOption = select.getFirstSelectedOption()
+                                            .getText();
+
+        softAssert.assertEquals(actualSecilenOption,expectedOption);
+
         //15. Dropdown menude 16 option bulundugunu dogrulayin.
+
+        int expectedOptionSayisi = 16;
+        int actualOptionSayisi = select.getOptions().size();
+
+        softAssert.assertEquals(actualOptionSayisi,expectedOptionSayisi);
+
+
         //16. Dropdown menude "Canada (dollar)" bulunduğunu dogrulayin
+
+        expectedOption = "Canada (dollar)";
+
+        List<WebElement> optionElementleriList = select.getOptions();
+
+        List<String> optionYazilariList = ReusableMethods.stringListeyeCevir(optionElementleriList);
+
+        softAssert.assertTrue(optionYazilariList.contains(expectedOption));
+
 
         softAssert.assertAll();
         //17. Sayfayi kapatin
